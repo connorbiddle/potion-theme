c909f90af26ee470f08d40a5cbeabb5127e2faa5
Connor <connor.biddle@hotmail.com>
2021-08-19 12:05:57 +0100

    Built search results template.

---
+++ search.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Template Name: Search Page
+ * 
+ * The search page template.
+ * 
+ * @package Potion
+ */
+?>
+
+<?php
+  get_header();
+  $category_image = get_the_post_thumbnail_url();
+?>
+
+<section class="showcase showcase--page">
+  <?php if ( $category_image ): ?>
+    <img class="showcase__image" src="<?php echo $category_image; ?>" alt="">
+  <?php endif; ?>
+  <div class="container--narrow showcase__contents">
+    <h2 class="showcase__title">
+      Results for &ldquo;<?php the_search_query(); ?>&rdquo;
+    </h2>
+  </div>
+</section>
+
+<main class="main">
+  <div class="container--narrow">
+    <?php
+      if ( have_posts() ):
+        while ( have_posts() ):
+          the_post();
+          get_template_part( 'template-parts/content/content' );
+        endwhile;
+        the_posts_pagination();
+      else:
+        get_template_part( 'template-parts/content/content-none' );
+      endif;
+    ?>
+  </div>
+</main>
+
+<?php get_footer(); ?>
